09ce1f3700a62ddcb064db0bfd054b454afc08a1
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-08-15 14:50:24 +0000

    Clean slate

---
+++ downloader.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 14:49:03 2020
+
+@author: Admin
+"""
+
---
+++ productClass.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 14:49:35 2020
+
+@author: Admin
+"""
+
