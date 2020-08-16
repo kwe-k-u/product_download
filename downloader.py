423d388a93df0bf76aa23c1c8c6d66272fc6fb48
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-08-16 10:46:09 +0000

    Images can be downloaded and text files created

--- downloader.py
+++ downloader.py
@@ -1,22 +1,91 @@
 import os
-import productClass
+from productClass import *
+import urllib.request as url
 
 
-images = []
+images = ["samsung", "infinix", "twitter"]
 
 #This creates the directory in which the files will be saved
 def createDirectory(folder = "jumia"):
+    #Change the working directory
     os.chdir("C:\\Users\\Admin\\Desktop")
+
+    #Create the directory if it doesn't already exist
     if os.path.exists( os.path.join(os.getcwd(),"images\\{0}".format(folder)) ):
        print("exists")
     else:
         os.makedirs( os.path.join(os.getcwd(),"images\\{0}".format(folder)) )
 
+    path = os.path.join(os.getcwd(),"images\\{0}".format(folder))
+    os.chdir(path)
+    createFile("highlights","something good about product")
+
+
+
+
+
+    #Downloads and saves the images from the product object
+def downloadImage(product):
+    path = os.getcwd()
+    #Looping through the image urls to download and save the images
+    for index in product.getImages():
+        url.urlretrieve(product.getImages()[index], path + "\\" + str(index) +".jpg")
+
+    #Create a text file to hold the product description
+
+    #Create a text file to hold the product highlights
+
+
+
+
+
+# =============================================================================
+# def createFile(fileName, text):
+# =============================================================================
+def createFile(title, description):
+    file = open("{0}.txt".format(title), "w")
+    file.write(description)
+    file.close()
+
+
+
+
+
 #This runs the program
 def main():
-    createDirectory()
+    print("Process beginning")
+
+    #get the product classes from the server
+    print("JSON data received") #add error checking for if it fails
+
+    #create the directory to hold the files and save the files
+    for im in images:
+        createDirectory(im)
+
+
     print("done")
 
 
-main()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# Running the program
+# =============================================================================
+
+main()
--- productClass.py
+++ productClass.py
@@ -1,7 +1,24 @@
 class productClass:
 
-    def __init__(self, productName, description, imageUrl):
+    def __init__(self, productName, description):
         self.name = productName
         self.description = description
-        self.imageArray = imageUrl
-        self.location = productName
+        self.imageArray = { 1: "king",
+                           2: "kinges"
+                }#todo do the filling later
+
+
+    #returns the product name
+    def getName(self):
+        return self.name
+
+    #return description
+    def getDescription(self):
+        return self.description
+
+    #return images
+    def getImages(self, index = -1):
+        if index == -1:
+            return self.imageArray
+        else:
+            return self.imageArray[index]
